a9ba50f3c9f6fb096683a9f1d604a6abcfce541d
Onkar Kshirsagar <Onkar Kshirsagar@DESKTOP-P7NAA1L>
2021-08-10 11:59:54 +0530

    Changes Made By Onkar

--- src/Test/Velocity_test.java
+++ src/Test/Velocity_test.java
@@ -5,7 +5,8 @@ public class Velocity_test {
 	public static void main(String[] args) {
 		
 		System.out.println("Good morning");
-
+		System.out.println("Good Morning ALL");
+		System.out.println("Changes Made");
 	}
 
 }
